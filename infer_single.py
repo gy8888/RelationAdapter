5fc8b940c9bde812be05c1d1fd8fbf529290d51f
Yan Gong <2458999423@qq.com>
2025-05-30 15:40:27 +0800

    Initial commit

---
+++ infer_single.py
@@ -0,0 +1,253 @@
+import os
+import glob
+import numpy as np
+from PIL import Image
+import json
+from tqdm import tqdm  # for displaying progress bar
+
+import torch
+import torch.nn as nn
+
+# from pipeline_flux_ipa import FluxPipeline
+from src.pipeline_pe_clone import FluxPipeline
+from transformer_flux import FluxTransformer2DModel
+from attention_processor import IPAFluxAttnProcessor2_0
+from transformers import AutoProcessor, SiglipVisionModel
+
+# ======================== Image Resize Function ===========================
+def resize_img(input_image, pad_to_regular=False, target_long_side=512, mode=Image.BILINEAR):
+    w, h = input_image.size
+    aspect_ratios = [(3, 4), (4, 3), (1, 1), (16, 9), (9, 16)]
+
+    if pad_to_regular:
+        img_ratio = w / h
+
+        # Find the aspect ratio closest to the original image
+        best_ratio = min(
+            aspect_ratios,
+            key=lambda r: abs((r[0] / r[1]) - img_ratio)
+        )
+
+        target_w_ratio, target_h_ratio = best_ratio
+        if w / h >= target_w_ratio / target_h_ratio:
+            target_w = w
+            target_h = int(w * target_h_ratio / target_w_ratio)
+        else:
+            target_h = h
+            target_w = int(h * target_w_ratio / target_h_ratio)
+
+        # Create white background and paste the image centered
+        padded_img = Image.new("RGB", (target_w, target_h), (255, 255, 255))
+        offset_x = (target_w - w) // 2
+        offset_y = (target_h - h) // 2
+        padded_img.paste(input_image, (offset_x, offset_y))
+        input_image = padded_img
+        w, h = input_image.size
+
+    # Resize while keeping aspect ratio
+    scale_ratio = target_long_side / max(w, h)
+    new_w = round(w * scale_ratio)
+    new_h = round(h * scale_ratio)
+    input_image = input_image.resize((new_w, new_h), mode)
+
+    return input_image
+
+# ======================== MLP Projection Module ===========================
+class MLPProjModel(torch.nn.Module):
+    def __init__(self, cross_attention_dim=768, id_embeddings_dim=512, num_tokens=4):
+        super().__init__()
+        
+        self.cross_attention_dim = cross_attention_dim
+        self.num_tokens = num_tokens
+        
+        self.proj = torch.nn.Sequential(
+            torch.nn.Linear(id_embeddings_dim, id_embeddings_dim*2),
+            torch.nn.GELU(),
+            torch.nn.Linear(id_embeddings_dim*2, cross_attention_dim*num_tokens),
+        )
+        self.norm = torch.nn.LayerNorm(cross_attention_dim)
+        
+    def forward(self, id_embeds):
+        x = self.proj(id_embeds)
+        x = x.reshape(-1, self.num_tokens, self.cross_attention_dim)
+        x = self.norm(x)
+        return x
+
+# ======================== IPAdapter Wrapper ===========================
+class IPAdapter:
+    def __init__(self, sd_pipe, image_encoder_path, ip_ckpt, device, num_tokens=4):
+        self.device = device
+        self.image_encoder_path = image_encoder_path
+        self.ip_ckpt = ip_ckpt
+        self.num_tokens = num_tokens
+
+        self.pipe = sd_pipe.to(self.device)
+        self.set_ip_adapter()
+
+        # Load image encoder
+        self.image_encoder = SiglipVisionModel.from_pretrained(image_encoder_path).to(self.device, dtype=torch.bfloat16)
+        self.clip_image_processor = AutoProcessor.from_pretrained(self.image_encoder_path)
+        
+        # Initialize image projection model
+        self.image_proj_model = self.init_proj()
+
+        self.load_ip_adapter()
+
+    def init_proj(self):
+        image_proj_model = MLPProjModel(
+            cross_attention_dim=self.pipe.transformer.config.joint_attention_dim, # 4096
+            id_embeddings_dim=1152, 
+            num_tokens=self.num_tokens,
+        ).to(self.device, dtype=torch.bfloat16)
+        
+        return image_proj_model
+    
+    def set_ip_adapter(self):
+        transformer = self.pipe.transformer
+        ip_attn_procs = {}  # total 57 layers: 19 + 38
+        for name in transformer.attn_processors.keys():
+            if name.startswith("transformer_blocks.") or name.startswith("single_transformer_blocks"):
+                ip_attn_procs[name] = IPAFluxAttnProcessor2_0(
+                    hidden_size=transformer.config.num_attention_heads * transformer.config.attention_head_dim,
+                    cross_attention_dim=transformer.config.joint_attention_dim,
+                    num_tokens=self.num_tokens,
+                ).to(self.device, dtype=torch.bfloat16)
+            else:
+                ip_attn_procs[name] = transformer.attn_processors[name]
+    
+        transformer.set_attn_processor(ip_attn_procs)
+    
+    def load_ip_adapter(self):
+        state_dict = torch.load(self.ip_ckpt, map_location="cpu")
+
+        # ---- 1. Load weights for image_proj_model ----
+        image_proj_state_dict = state_dict["image_proj"]
+        if list(image_proj_state_dict.keys())[0].startswith("module."):
+            # Remove DataParallel/DDP prefix
+            image_proj_state_dict = {
+                k.replace("module.", ""): v for k, v in image_proj_state_dict.items()
+            }
+        self.image_proj_model.load_state_dict(image_proj_state_dict, strict=True)
+
+        # ---- 2. Load weights for ip_adapter (attn_processors) ----
+        ip_adapter_state_dict = state_dict["ip_adapter"]
+        if list(ip_adapter_state_dict.keys())[0].startswith("module."):
+            ip_adapter_state_dict = {
+                k.replace("module.", ""): v for k, v in ip_adapter_state_dict.items()
+            }
+
+        ip_layers = torch.nn.ModuleList(self.pipe.transformer.attn_processors.values())
+        ip_layers.load_state_dict(ip_adapter_state_dict, strict=False)
+
+    @torch.inference_mode()
+    def get_image_embeds(self, pil_image=None, clip_image_embeds=None):
+        if pil_image is not None:
+            if isinstance(pil_image, Image.Image):
+                pil_image = [pil_image]
+            clip_image = self.clip_image_processor(images=pil_image, return_tensors="pt").pixel_values
+            clip_image_embeds = self.image_encoder(clip_image.to(self.device, dtype=self.image_encoder.dtype)).pooler_output
+            clip_image_embeds = clip_image_embeds.to(dtype=torch.bfloat16)
+        else:
+            clip_image_embeds = clip_image_embeds.to(self.device, dtype=torch.bfloat16)
+        image_prompt_embeds = self.image_proj_model(clip_image_embeds)
+        return image_prompt_embeds
+    
+    def set_scale(self, scale):
+        for attn_processor in self.pipe.transformer.attn_processors.values():
+            if isinstance(attn_processor, IPAFluxAttnProcessor2_0):
+                attn_processor.scale = scale
+
+    def generate(
+        self,
+        condition_image=None,
+        pil_image=None,                  # supports list or tuple of two PIL images
+        clip_image_embeds=None,
+        prompt=None,
+        scale=1.0,
+        num_samples=1,
+        seed=None,
+        guidance_scale=3.5,
+        num_inference_steps=24,
+        **kwargs,
+    ):
+        self.set_scale(scale)
+
+        # Support case with two input images
+        if isinstance(pil_image, (list, tuple)) and len(pil_image) == 2:
+            image_prompt_embeds1 = self.get_image_embeds(pil_image=pil_image[0])
+            image_prompt_embeds2 = self.get_image_embeds(pil_image=pil_image[1])
+            image_prompt_embeds = torch.cat([image_prompt_embeds1, image_prompt_embeds2], dim=0)
+        else:
+            image_prompt_embeds = self.get_image_embeds(
+                pil_image=pil_image, clip_image_embeds=clip_image_embeds
+            )
+
+        generator = torch.Generator(self.device).manual_seed(seed) if seed is not None else None
+
+        images = self.pipe(
+            prompt=prompt,
+            condition_image=condition_image,
+            image_emb=image_prompt_embeds,
+            guidance_scale=guidance_scale,
+            num_inference_steps=num_inference_steps,
+            generator=generator,
+            **kwargs,
+        ).images
+
+        return images
+
+# ======================== Parameter Setup ===========================
+BASE_MODEL_PATH = '../models/FLUX.1-dev'
+IMAGE_ENCODER_PATH = '../models/siglip-so400m-patch14-384'
+IPADAPTER_PATH = '/opt/liblibai-models/user-workspace2/users/gy/ipadapter/PhotoDoodle/outputs-four-gpu/my_model/checkpoint-100000-ip-adapter/ip_adapter-100000.bin'
+LORA_WEIGHTS_PATH = "/opt/liblibai-models/user-workspace2/users/gy/ipadapter/PhotoDoodle/outputs-four-gpu/my_model/checkpoint-100000-lora"
+LORA_WEIGHTS_FILE = "pytorch_lora_weights.safetensors"
+DEVICE = "cuda:5"
+
+# ========== Sample Image Paths ==========
+cond1_path = "../Data_doodle_ipa_test_seen/1-TeethAlignment/Group1/000001-1.jpg"
+cond2_path = "../Data_doodle_ipa_test_seen/1-TeethAlignment/Group1/000001-2.jpg"
+source_path = "../Data_doodle_ipa_test_seen/1-TeethAlignment/Group1/000001-3.jpg"
+prompt = "Align the teeth and enhance teeth whiteness"
+
+# ======================== Model Setup ===========================
+transformer = FluxTransformer2DModel.from_pretrained(
+    BASE_MODEL_PATH, subfolder="transformer", torch_dtype=torch.bfloat16
+)
+pipe = FluxPipeline.from_pretrained(
+    BASE_MODEL_PATH, transformer=transformer, torch_dtype=torch.bfloat16
+)
+
+pipe.load_lora_weights(LORA_WEIGHTS_PATH, weight_name=LORA_WEIGHTS_FILE)
+pipe.fuse_lora()
+pipe.unload_lora_weights()
+print("LoRA weights loaded ✔️")
+
+ip_model = IPAdapter(pipe, IMAGE_ENCODER_PATH, IPADAPTER_PATH, device=DEVICE, num_tokens=128)
+print("IP-Adapter initialized ✔️")
+
+# ======================== Image Preparation ===========================
+image1 = Image.open(cond1_path).convert("RGB")
+image2 = Image.open(cond2_path).convert("RGB")
+image1 = resize_img(image1, pad_to_regular=True, target_long_side=512)
+image2 = resize_img(image2, pad_to_regular=True, target_long_side=512)
+
+condition_image = Image.open(source_path).convert("RGB")
+width, height = condition_image.size
+
+# ======================== Inference ===========================
+generated_images = ip_model.generate(
+    prompt=prompt,
+    condition_image=condition_image,
+    height=height,
+    width=width,
+    pil_image=[image1, image2],
+    scale=1.0,
+    seed=1000,
+)
+
+# ======================== Show Result ===========================
+# generated_images[0].show()  # 显示生成图像
+
+# （可选）保存结果
+generated_images[0].save("generated.jpg")
---
+++ src/jsonl_datasets.py
@@ -0,0 +1,206 @@
+import torch
+from PIL import Image
+from datasets import load_dataset
+from torchvision import transforms
+import random
+import os
+import numpy as np
+from transformers import AutoProcessor
+import torchvision.transforms.functional as F
+
+Image.MAX_IMAGE_PIXELS = None
+
+def make_train_dataset(args, tokenizer, accelerator=None):
+    if args.train_data_dir is not None:
+        print("loading dataset ... ")
+        dataset = load_dataset('json', data_files=args.train_data_dir)
+        base_path = os.path.dirname(os.path.abspath(args.train_data_dir))
+
+    column_names = dataset["train"].column_names
+
+    # 6. Get the column names for input/target.
+    if args.caption_column is None:
+        caption_column = column_names[0]
+        print(f"caption column defaulting to {caption_column}")
+    else:
+        caption_column = args.caption_column
+        if caption_column not in column_names:
+            raise ValueError(
+                f"`--caption_column` value '{args.caption_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            )
+    if args.source_column is None:
+        source_column = column_names[1]
+        print(f"source column defaulting to {source_column}")
+    else:
+        source_column = args.source_column
+        if source_column not in column_names:
+            raise ValueError(
+                f"`--source_column` value '{args.source_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            )
+    if args.target_column is None:
+        target_column = column_names[2]
+        print(f"target column defaulting to {target_column}")
+    else:
+        target_column = args.target_column
+        if target_column not in column_names:
+            raise ValueError(
+                f"`--target_column` value '{args.target_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            )
+    #gy修改 加两张条件图片作为columnname
+    if args.ipa_source_column is None:
+        ipa_source_column = column_names[3]
+        print(f"ipa-source column defaulting to {ipa_source_column}")
+    else:
+        ipa_source_column = args.ipa_source_column
+        if ipa_source_column not in column_names:
+            raise ValueError(
+                f"`--ipa_source_column` value '{args.ipa_source_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            )
+    if args.ipa_target_column is None:
+        ipa_target_column = column_names[4]
+        print(f"ipa-target column defaulting to {ipa_target_column}")
+    else:
+        ipa_target_column = args.ipa_target_column
+        if ipa_target_column not in column_names:
+            raise ValueError(
+                f"`--ipa_target_column` value '{args.ipa_target_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            )
+
+    #gy修改 这边要改成支持多桶训练  
+    def resize_long_side(img, target_long_side, interpolation=transforms.InterpolationMode.BILINEAR):
+        # 获取图像宽和高。注意：PIL 图像返回的是 (宽, 高)
+        w, h = img.size  
+        # 判断哪个边更长
+        if w >= h:
+            # 若宽边更长，则将宽设为目标尺寸，同时按比例调整高度
+            new_w = target_long_side
+            new_h = int(target_long_side * h / w)
+        else:
+            # 若高边更长，则将高设为目标尺寸，同时按比例调整宽度
+            new_h = target_long_side
+            new_w = int(target_long_side * w / h)
+        # 使用 functional.resize 完成尺寸调整，注意传入的是 (高度, 宽度)
+        return F.resize(img, (new_h, new_w), interpolation=interpolation)
+
+    train_transforms = transforms.Compose(
+        [
+            # 使用 Lambda 包装自定义的 resize_long_side 函数
+            transforms.Lambda(lambda img: resize_long_side(img, args.resolution, interpolation=transforms.InterpolationMode.BILINEAR)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]),
+        ]
+    )
+
+    tokenizer_clip = tokenizer[0]
+    tokenizer_t5 = tokenizer[1]
+
+    def tokenize_prompt_clip_t5(examples):
+        captions = []
+        for caption in examples[caption_column]:
+            if isinstance(caption, str):
+                captions.append(caption)                    
+            elif isinstance(caption, list):
+                captions.append(random.choice(caption))
+            else:
+                raise ValueError(
+                    f"Caption column `{caption_column}` should contain either strings or lists of strings."
+                )
+        text_inputs = tokenizer_clip(
+            captions,
+            padding="max_length",
+            max_length=77,
+            truncation=True,
+            return_length=False,
+            return_overflowing_tokens=False,
+            return_tensors="pt",
+        )
+        text_input_ids_1 = text_inputs.input_ids
+
+        text_inputs = tokenizer_t5(
+            captions,
+            padding="max_length",
+            max_length=512,
+            truncation=True,
+            return_length=False,
+            return_overflowing_tokens=False,
+            return_tensors="pt",
+        )
+        text_input_ids_2 = text_inputs.input_ids
+        return text_input_ids_1, text_input_ids_2
+
+    def preprocess_train(examples):
+        _examples = {}        
+
+        source_images = [Image.open(os.path.join(base_path, image)).convert("RGB") 
+                        for image in examples[source_column]]
+        target_images = [Image.open(os.path.join(base_path, image)).convert("RGB") 
+                        for image in examples[target_column]]
+
+        _examples["cond_pixel_values"] = [train_transforms(source) for source in source_images]
+        _examples["pixel_values"] = [train_transforms(image) for image in target_images]
+        _examples["token_ids_clip"], _examples["token_ids_t5"] = tokenize_prompt_clip_t5(examples)
+
+        # gy修改 加载clip pre-processor
+        # ————————————————————————————————————————————————————————
+        clip_image_processor = AutoProcessor.from_pretrained('../models/siglip-so400m-patch14-384')
+        ipa_source_images = [Image.open(os.path.join(base_path, image)).convert("RGB") 
+                        for image in examples[ipa_source_column]]
+        ipa_target_images = [Image.open(os.path.join(base_path, image)).convert("RGB") 
+                        for image in examples[ipa_target_column]]
+        _examples["ipa_source_images"] = [clip_image_processor(images=source, return_tensors="pt").pixel_values for source in ipa_source_images]
+        _examples["ipa_target_images"] = [clip_image_processor(images=image, return_tensors="pt").pixel_values for image in ipa_target_images]
+        
+        drop_image_embeds = [1 if random.random() < 0.05 else 0 for _ in examples[ipa_target_column]]
+        _examples["drop_image_embeds"] = drop_image_embeds
+        # print(f"ipa_source_images[0] shape: {_examples['ipa_source_images'][0].shape}")
+        # print(f"ipa_target_images[0] shape: {_examples['ipa_target_images'][0].shape}")
+
+        # —————————————————————————————————————————————————————————
+        return _examples
+
+    if accelerator is not None:
+        with accelerator.main_process_first():
+            train_dataset = dataset["train"].with_transform(preprocess_train)
+    else:
+        train_dataset = dataset["train"].with_transform(preprocess_train)
+
+    return train_dataset
+
+
+def collate_fn(examples):
+    cond_pixel_values = torch.stack([example["cond_pixel_values"] for example in examples])
+    cond_pixel_values = cond_pixel_values.to(memory_format=torch.contiguous_format).float()
+    target_pixel_values = torch.stack([example["pixel_values"] for example in examples])
+    target_pixel_values = target_pixel_values.to(memory_format=torch.contiguous_format).float()
+    # gy修改
+    # token_ids_clip = torch.stack([torch.tensor(example["token_ids_clip"]) for example in examples])
+    # token_ids_t5 = torch.stack([torch.tensor(example["token_ids_t5"]) for example in examples])
+    
+    token_ids_clip = torch.stack([
+        example["token_ids_clip"].clone().detach() if isinstance(example["token_ids_clip"], torch.Tensor)
+        else torch.tensor(example["token_ids_clip"])
+        for example in examples
+    ])
+    token_ids_t5 = torch.stack([
+        example["token_ids_t5"].clone().detach() if isinstance(example["token_ids_t5"], torch.Tensor)
+        else torch.tensor(example["token_ids_t5"])
+        for example in examples
+    ])
+
+    # gy修改
+    # ————————————————————————————————————————————————————————————————————————————
+    ipa_source_pixel_values = torch.cat([example["ipa_source_images"] for example in examples])
+    ipa_source_pixel_values = ipa_source_pixel_values.to(memory_format=torch.contiguous_format).float()
+    ipa_target_pixel_values = torch.cat([example["ipa_target_images"] for example in examples])
+    ipa_target_pixel_values = ipa_target_pixel_values.to(memory_format=torch.contiguous_format).float()
+    drop_image_embeds = [example["drop_image_embeds"] for example in examples]
+    return {
+        "cond_pixel_values": cond_pixel_values,
+        "pixel_values": target_pixel_values,
+        "text_ids_1": token_ids_clip,
+        "text_ids_2": token_ids_t5,
+        "ipa_source_pixel_values": ipa_source_pixel_values,
+        "ipa_target_pixel_values": ipa_target_pixel_values,
+        "drop_image_embeds": drop_image_embeds
+    }
+    # ————————————————————————————————————————————————————————————————————————————
